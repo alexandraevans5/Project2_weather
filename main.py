ae678cb79f9ebaf0630b6542cf0373bf81e89e56
alexandraevans5 <sasha.kleymenova@gmail.com>
2024-12-22 21:50:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-API_KEY = 'KlrnNrBdipA7L9DwvbwLgPYvrziVBHA1'
+API_KEY = "KlrnNrBdipA7L9DwvbwLgPYvrziVBHA1"
 BASE_URL = 'http://dataservice.accuweather.com/'
 
 
@@ -18,7 +18,7 @@ def get_weather():
     location_key_url = f"{BASE_URL}locations/v1/cities/geoposition/search?apikey={API_KEY}&q={latitude},{longitude}"
     location_response = requests.get(location_key_url)
 
-    if location_response.status_code != 200:
+    if not location_response:
         return jsonify({'error': 'Ошибка получения данных о местоположении'}), 500
 
     location_data = location_response.json()
@@ -46,14 +46,14 @@ def get_weather():
 def check_bad_weather(conds):
     result = True
 
-    if conds['temperature'] < -15 or conds['temperature'] > 35:
+    if float(conds['temperature']) < -15 or float(conds['temperature']) > 35:
         result = False
-    if conds['humidity'] > 85:
-        result = False
-    if conds['wind_speed'] > 50:
-        result = False
-    if conds['precipitation_probability'] > 70:
-        result = False
+    # if conds['humidity'] > 85:
+    #     result = False
+    # if conds['wind_speed'] > 50:
+    #     result = False
+    # if conds['precipitation_probability'] > 70:
+    #     result = False
 
     return result
 
@@ -62,31 +62,21 @@ def simple_get_weather(city):
     location_key_url = f"{BASE_URL}locations/v1/cities/search?apikey={API_KEY}&q={city}"
     location_response = requests.get(location_key_url)
 
-    from pprint import pprint
-    pprint(location_response.json())
-    if location_response.status_code != 200:
-        return
-
     location_data = location_response.json()
     location_key = location_data[0]['Key']
 
     weather_url = f"{BASE_URL}forecasts/v1/daily/1day/{location_key}?apikey={API_KEY}&metric=true"
     weather_response = requests.get(weather_url)
 
-    pprint(weather_response.json())
-    if weather_response.status_code != 200:
-        return
-
     forecast = weather_response.json()['DailyForecasts'][0]
 
     try:
         result = {
             'temperature': forecast['Temperature']['Maximum']['Value'],
-            'humidity': forecast['Day']['Humidity'],
-            'wind_speed': forecast['Day']['Wind']['Speed']['Value'],
-            'precipitation_probability': forecast['Day']['RainProbability']
+            # 'humidity': forecast['Day']['Humidity'],
+            #'wind_speed': forecast['Day']['Wind']['Speed']['Value'],
+            #'precipitation_probability': forecast['Day']['RainProbability']
         }
-
         return result
     except KeyError:
         return
@@ -99,25 +89,31 @@ def index():
         end_point = request.form.get("end_point")
 
         start_weather = simple_get_weather(start_point)
-        if start_weather is None:
-            start_weather = "Ошибка получения данных"
-            start_weather_conds = ""
-        else:
-            start_weather_conds = check_bad_weather(start_weather)
+        start_weather_text = f"Температура: {start_weather["temperature"]}"
+        # if start_weather is None:
+        #     start_weather = "Ошибка получения данных"
+        #     start_weather_conds = ""
+        # else:
+        #     start_weather_conds = check_bad_weather(start_weather)
+        start_weather_conds = check_bad_weather(start_weather)
+        start_weather_conds = "Погода хорошая" if start_weather_conds else "Погода плохая"
 
         end_weather = simple_get_weather(end_point)
+        end_weather_text = f"Температура: {end_weather["temperature"]}"
         if end_weather is None:
             end_weather = "Ошибка получения данных"
             end_weather_conds = ""
         else:
             end_weather_conds = check_bad_weather(end_weather)
 
-        return render_template("result.html", start_point=start_point, end_point=end_point, start_weather=start_weather,
-                               end_weather=end_weather, start_weather_conds=start_weather_conds,
+        end_weather_conds = "Погода хорошая" if end_weather_conds else "Погода плохая"
+        return render_template("result.html", start_point=start_point, end_point=end_point, start_weather=start_weather_text,
+                               end_weather=end_weather_text, start_weather_conds=start_weather_conds,
                                end_weather_conds=end_weather_conds)
 
     return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4002)
+    app.run(debug=True, port=4003)
+
